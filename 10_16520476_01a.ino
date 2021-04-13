2b1a824753c146f5f9f2394d313db97cb1a213ba
trizzosgl <81562249+trizzosgl@users.noreply.github.com>
2021-04-13 10:24:38 +0700

    Add files via upload

---
+++ 10_16520476_01a.ino
@@ -0,0 +1,10 @@
+void setup()
+{
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  delay(10); // Delay a little bit to improve simulation performance
+}
---
+++ 10_16520476_01c.ino
@@ -0,0 +1,17 @@
+int counter;
+
+void setup()
+{
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  for (counter = 0; counter < 3; ++counter) {
+    digitalWrite(7, HIGH);
+    delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(7, LOW);
+    delay(1000); // Wait for 1000 millisecond(s)
+  }
+  delay(5000); // Wait for 5000 millisecond(s)
+}
